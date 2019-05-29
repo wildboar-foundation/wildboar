ecea84519a4563fbe73b77069bea7414ed18568c
Isak Karlsson <isak.karlsson@gmail.com>
2019-05-29 17:42:49 +0200

    Improve build scripts

--- build-wheel.sh
+++ build-wheel.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-cmd="wheel /io/ -w /io/wheelhouse/"
+cmd="wheel --no-deps /io/ -w /io/wheelhouse/"
 
 echo "Building for Python 3.6"
+echo " - installing requirements"
+/opt/python/cp36-cp36m/bin/pip install -r "/io/requirements.txt"
+echo " - building"
 /opt/python/cp36-cp36m/bin/pip $cmd
 
 echo "Building for Python 3.7"
+echo " - installing requirements"
+/opt/python/cp37-cp37m/bin/pip install -r "/io/requirements.txt"
+echo " - building"
 /opt/python/cp37-cp37m/bin/pip $cmd
 
 for whl in /io/wheelhouse/*.whl; do
