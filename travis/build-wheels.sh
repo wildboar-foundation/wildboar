333e3f9de469c5414af315dc3996f44d6c010ac1
Isak Karlsson <isak.karlsson@gmail.com>
2020-02-27 17:43:21 +0100

    Fix travis integration

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,7 +5,7 @@ set -e -x
 yum install -y atlas-devel
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/cp3*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -16,7 +16,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in /opt/python/cp3*/bin/; do
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
