be38ff9e4765a1935ccca05ac88bf165a734e797
Isak Karlsson <isak.karlsson@gmail.com>
2020-02-27 17:36:40 +0100

    Add travis integration

--- build-wheel.sh
+++ build-wheel.sh
@@ -13,6 +13,12 @@ echo " - installing requirements"
 echo " - building"
 /opt/python/cp37-cp37m/bin/pip $cmd
 
+echo "Building for Python 3.7"
+echo " - installing requirements"
+/opt/python/cp38-cp38m/bin/pip install -r "/io/requirements.txt"
+echo " - building"
+/opt/python/cp38-cp38m/bin/pip $cmd
+
 for whl in /io/wheelhouse/*.whl; do
     auditwheel repair "$whl" --plat manylinux2010_x86_64 -w /io/wheelhouse/
 done
