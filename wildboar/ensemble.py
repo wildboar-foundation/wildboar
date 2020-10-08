b3cbd6ede9611ffdea804d5fbd3e58e44b89590a
Isak Samsten <isak@samsten.se>
2020-10-08 13:45:19 +0200

    Add `IsolationShapeletForest` for time series outlier detection

--- wildboar/ensemble.py
+++ wildboar/ensemble.py
@@ -17,13 +17,15 @@
 from abc import ABCMeta, abstractmethod
 
 import numpy as np
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, OutlierMixin
 from sklearn.base import ClassifierMixin
 from sklearn.base import RegressorMixin
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble._bagging import BaseBagging
 from sklearn.utils import check_array
 from sklearn.utils import check_random_state
+from sklearn.utils.fixes import _joblib_parallel_args
 from sklearn.utils.validation import check_is_fitted
 
 from .tree import ShapeletTreeClassifier, ExtraShapeletTreeClassifier, ExtraShapeletTreeRegressor
@@ -32,7 +34,8 @@ from .tree import ShapeletTreeRegressor
 __all__ = ["ShapeletForestClassifier",
            "ExtraShapeletTreesClassifier",
            "ShapeletForestRegressor",
-           "ExtraShapeletTreesRegressor"]
+           "ExtraShapeletTreesRegressor",
+           "IsolationShapeletForest"]
 
 
 class BaseShapeletForest(BaseEstimator, metaclass=ABCMeta):
@@ -321,3 +324,146 @@ class ExtraShapeletTreesRegressor(ShapeletForestRegressor):
             metric_params=self.metric_params,
             random_state=random_state,
         )
+
+
+class IsolationShapeletForest(OutlierMixin, BaseBagging):
+    def __init__(self, *,
+                 n_estimators=100,
+                 bootstrap=False,
+                 n_jobs=None,
+                 metric='euclidean',
+                 min_shapelet_size=0,
+                 max_shapelet_size=1,
+                 min_samples_split=2,
+                 metric_params=None,
+                 random_state=None):
+        super(IsolationShapeletForest, self).__init__(
+            base_estimator=ExtraShapeletTreeRegressor(),
+            bootstrap=bootstrap,
+            bootstrap_features=False,
+            n_jobs=n_jobs,
+            n_estimators=n_estimators,
+            random_state=random_state
+        )
+        self.estimator_params = (
+            "min_samples_split", "min_shapelet_size",
+            "max_shapelet_size", "metric", "metric_params"
+        )
+        self.metric = metric
+        self.metric_params = metric_params
+        self.min_shapelet_size = min_shapelet_size
+        self.max_shapelet_size = max_shapelet_size
+        self.min_samples_split = min_samples_split
+        self.n_dims = None
+        self.n_timestep = None
+        self.offset_ = -0.5
+
+    def _validate_x_predict(self, x, check_input):
+        if x.ndim < 2 or x.ndim > 3:
+            raise ValueError("illegal input dimensions X.ndim ({})".format(
+                x.ndim))
+        if self.n_dims > 1 and x.ndim != 3:
+            raise ValueError("illegal input dimensions X.ndim != 3")
+        if x.shape[-1] != self.n_timestep:
+            raise ValueError("illegal input shape ({} != {})".format(
+                x.shape[-1], self.n_timestep))
+        if x.ndim > 2 and x.shape[1] != self.n_dims:
+            raise ValueError("illegal input shape ({} != {}".format(
+                x.shape[1], self.n_dims))
+        if check_input:
+            x = check_array(x, dtype=np.float64, allow_nd=True, order="C")
+        if x.dtype != np.float64 or not x.flags.contiguous:
+            x = np.ascontiguousarray(x, dtype=np.float64)
+        x = x.reshape(x.shape[0], self.n_dims * self.n_timestep)
+        return x
+
+    def _parallel_args(self):
+        return _joblib_parallel_args(prefer="threads")
+
+    def _set_oob_score(self, x, y):
+        raise NotImplementedError("OOB score not supported")
+
+    def fit(self, x, y=None, sample_weight=None, check_input=True):
+        random_state = check_random_state(self.random_state)
+        if check_input:
+            x = check_array(x, dtype=np.float64, allow_nd=True, order="C")
+
+        if x.ndim < 2 or x.ndim > 3:
+            raise ValueError("illegal input dimension")
+
+        n_samples = x.shape[0]
+        self.n_timestep = x.shape[-1]
+        if x.ndim > 2:
+            n_dims = x.shape[1]
+        else:
+            n_dims = 1
+
+        self.n_dims = n_dims
+
+        if x.dtype != np.float64 or not x.flags.contiguous:
+            x = np.ascontiguousarray(x, dtype=np.float64)
+
+        if n_dims > 1:
+            self.base_estimator_.force_dim = n_dims
+
+        x = x.reshape(n_samples, n_dims * self.n_timestep)
+        y = random_state.uniform(size=x.shape[0])
+        max_depth = int(np.ceil(np.log2(max(x.shape[0], 2))))
+        super(IsolationShapeletForest, self)._fit(
+            x, y, max_samples=x.shape[0], max_depth=max_depth, sample_weight=sample_weight)
+        return self
+
+    def predict(self, x):
+        check_is_fitted(self)
+        x = self._validate_x_predict(x, check_input=True)
+        is_inlier = np.ones(x.shape[0])
+        is_inlier[self.decision_function(x) < 0] = -1
+        return is_inlier
+
+    def decision_function(self, x):
+        check_is_fitted(self)
+        x = self._validate_x_predict(x, check_input=True)
+        return self.score_samples(x) - self.offset_
+
+    def score_samples(self, X):
+        # From: https://github.com/scikit-learn/scikit-learn/blob/0fb307bf39bbdacd6ed713c00724f8f871d60370/sklearn/ensemble/_iforest.py#L411
+        n_samples = X.shape[0]
+
+        depths = np.zeros(n_samples, order="f")
+
+        for tree in self.estimators_:
+            leaves_index = tree.apply(X)
+            node_indicator = tree.decision_path(X)
+            n_samples_leaf = tree.n_node_samples[leaves_index]
+
+            depths += (
+                    np.ravel(node_indicator.sum(axis=1))
+                    + _average_path_length(n_samples_leaf)
+                    - 1.0
+            )
+        scores = 2 ** (
+                -depths
+                / (len(self.estimators_)
+                   * _average_path_length(np.array([X.shape[0]])))
+        )
+        return -scores
+
+
+def _average_path_length(n_samples_leaf):
+    # From: https://github.com/scikit-learn/scikit-learn/blob/0fb307bf39bbdacd6ed713c00724f8f871d60370/sklearn/ensemble/_iforest.py#L480
+    n_samples_leaf_shape = n_samples_leaf.shape
+    n_samples_leaf = n_samples_leaf.reshape((1, -1))
+    average_path_length = np.zeros(n_samples_leaf.shape)
+
+    mask_1 = n_samples_leaf <= 1
+    mask_2 = n_samples_leaf == 2
+    not_mask = ~np.logical_or(mask_1, mask_2)
+
+    average_path_length[mask_1] = 0.
+    average_path_length[mask_2] = 1.
+    average_path_length[not_mask] = (
+            2.0 * (np.log(n_samples_leaf[not_mask] - 1.0) + np.euler_gamma)
+            - 2.0 * (n_samples_leaf[not_mask] - 1.0) / n_samples_leaf[not_mask]
+    )
+
+    return average_path_length.reshape(n_samples_leaf_shape)
