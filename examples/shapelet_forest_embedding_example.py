e73d09c97c8a632b90caed151e0a0bc2c02fe230
Isak Samsten <isak@samsten.se>
2021-01-23 12:46:54 +0100

    [FEATURE] Improve ShapeletForestEmbedding example

--- examples/shapelet_forest_embedding_example.py
+++ examples/shapelet_forest_embedding_example.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, cross_validate
-from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.model_selection import cross_validate
+from sklearn.pipeline import make_pipeline
 
 from wildboar.datasets import load_dataset
 from wildboar.ensemble import ShapeletForestEmbedding
@@ -25,4 +27,25 @@ pipe = make_pipeline(
     LogisticRegression(solver="newton-cg", random_state=random_state),
 )
 cv = cross_validate(pipe, x, y, cv=5, scoring="accuracy", n_jobs=1)
-print(1 - np.mean(cv["test_score"]))
+err = 1 - np.mean(cv["test_score"])
+print(err)
+
+pca = make_pipeline(
+    ShapeletForestEmbedding(
+        metric="scaled_euclidean",
+        sparse_output=False,
+        max_depth=5,
+        random_state=random_state,
+    ),
+    PCA(n_components=2, random_state=random_state),
+)
+p = pca.fit_transform(x)
+var = pca.steps[1][1].explained_variance_ratio_
+
+labels, index = np.unique(y, return_inverse=True)
+colors = plt.cm.rainbow(np.linspace(0, 1, len(labels)))
+plt.scatter(p[:, 0], p[:, 1], color=colors[index, :])
+plt.xlabel("Component 1 (%.2f variance explained)" % var[0])
+plt.ylabel("Component 2 (%.2f variance explained)" % var[1])
+plt.title("PCA with 2 components  (E_{out} = %.2f )" % err)
+plt.show()
