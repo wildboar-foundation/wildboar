b481b4607f1f513e20f5d57d1a5f1a640c03706b
isaksamsten <isaksamsten@users.noreply.github.com>
2021-04-16 20:31:06 +0000

    deploy: 515c887aeba21c7b3188d7fe6d0ac0aa0c6628f9

--- 1.1.X/_sources/guide/unsupervised.rst.txt
+++ 1.1.X/_sources/guide/unsupervised.rst.txt
@@ -4,5 +4,6 @@ Unsupervised learning
 
 Outlier detection
 =================
+Wildboar includes one estimator for outlier detection: the ``IsolationShapeletForest``.
 
 
--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.9.dev39+g1f3b134',
+    VERSION: '1.1.0rc1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
