1731a902e0e27846dc3215ffa61462cf94a945a4
Isak Samsten <isak@samsten.se>
2021-10-26 14:36:40 +0200

    Update documentation

--- docs/examples/code/interval_embedding.py
+++ docs/examples/code/interval_embedding.py
@@ -1,5 +1,5 @@
 import matplotlib.pylab as plt
-import pandas as pd
+import numpy as np
 
 from wildboar.datasets import load_dataset
 from wildboar.embed import IntervalEmbedding
@@ -10,15 +10,17 @@ fixed = IntervalEmbedding(n_interval=30, summarizer="auto", intervals="fixed")
 x_t = fixed.fit_transform(x)
 labels = ["%s" % start for (dim, (start, length, _)) in fixed.embedding_.features]
 n_features = x_t.shape[1]
-df = pd.DataFrame(
-    {
-        "mean": x_t[0, 0:n_features:3],
-        "variance": x_t[0, 1:n_features:3],
-        "slope": x_t[0, 2:n_features:3],
-    },
-    index=labels,
-)
-fig, ax = plt.subplots(nrows=2)
+
+fig, ax = plt.subplots(nrows=4, figsize=(5.5, 7))
 ax[0].plot(x[0])
-df.plot(kind="bar", ax=ax[1])
+ax[0].title.set_text("Time series")
+colors = plt.cm.rainbow(np.linspace(0, 1, 3))
+titles = ["Mean", "Variance", "Slope"]
+for i in range(3):
+    ax[i + 1].bar(labels, x_t[0, i:n_features:3], color=colors[i, :])
+    plt.setp(ax[i + 1].get_xticklabels(), rotation="vertical", ha="center")
+    ax[i + 1].title.set_text(titles[i])
+
+plt.tight_layout()
+# plt.xlim([0, 150])
 plt.savefig("../fig/interval.png")
