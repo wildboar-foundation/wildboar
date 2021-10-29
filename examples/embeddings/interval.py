75c64089ffc3363f66e201af74163af413b7ee8d
Isak Samsten <isak@samsten.se>
2021-10-29 13:18:02 +0200

    Update interval example

--- examples/embeddings/interval.py
+++ examples/embeddings/interval.py
@@ -1,9 +1,15 @@
+from timeit import default_timer as timer
+
 import catch22
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import RidgeClassifierCV
+from sklearn.pipeline import make_pipeline
 
 from wildboar.datasets import load_dataset
 from wildboar.embed import IntervalEmbedding
 
+x, y = load_dataset("GunPoint")
 x_train, x_test, y_train, y_test = load_dataset("GunPoint", merge_train_test=False)
 
 
@@ -26,12 +32,19 @@ names = [
     if callable(f) and f.__name__ != "catch22_all"
 ]
 
+selection = np.arange(x.shape[0])
 i = IntervalEmbedding(
     n_interval=1,
     summarizer=summarizers,
     random_state=123,
 )
-x_t = i.fit_transform(x_train[[0, 1, 13]])
+
+
+start = timer()
+x_t = i.fit_transform(x[selection])
+end = timer()
+print("non-native: ", end - start)
+
 df_expected = pd.DataFrame(x_t, columns=names)
 
 i = IntervalEmbedding(
@@ -39,24 +52,29 @@ i = IntervalEmbedding(
     summarizer="catch22",
     random_state=123,
 )
-x_t = i.fit_transform(x_train[[0, 1, 13]])
+start = timer()
+x_t = i.fit_transform(x[selection])
+end = timer()
+print("native: ", end - start)
 df_actual = pd.DataFrame(x_t, columns=names)
 
-with pd.option_context(
-    "display.max_rows", None, "display.max_columns", None, "display.width", None
-):
-    print(df_expected[names[0:22]])
-    print(df_actual[names[0:22]])
+# df_expected["SB_BinaryStats_mean_longstretch1"] += 1
+pd.testing.assert_frame_equal(df_expected, df_actual)
+# with pd.option_context(
+#     "display.max_rows", None, "display.max_columns", None, "display.width", None
+# ):
+#     #print(df_expected - df_actual)
 
-
-#
-# pipe = make_pipeline(
-#     IntervalEmbedding(
-#         n_interval=1,
-#         intervals="fixed",
-#         summarizer=summarizers,
-#     ),
-#     RandomForestClassifier(),
-# )
-# pipe.fit(x_train, y_train)
-# print(pipe.score(x_test, y_test))
+pipe = make_pipeline(
+    IntervalEmbedding(
+        n_interval=1000,
+        intervals="random",
+        summarizer="catch22",
+        min_size=0.1,
+        max_size=0.2,
+        n_jobs=-1,
+    ),
+    RidgeClassifierCV(),
+)
+pipe.fit(x_train, y_train)
+print(pipe.score(x_test, y_test))
