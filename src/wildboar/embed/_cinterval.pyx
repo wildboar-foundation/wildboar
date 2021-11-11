79348751b0e07ed5671b44103f58dbed06639c0e
Isak Samsten <isak@samsten.se>
2021-11-11 11:11:47 +0100

    Improve interval single feature performance

--- src/wildboar/embed/_cinterval.pyx
+++ src/wildboar/embed/_cinterval.pyx
@@ -20,7 +20,7 @@ cimport numpy as np
 
 import numpy as np
 
-from libc.math cimport FP_INFINITE, fpclassify
+from libc.math cimport NAN
 from libc.stdlib cimport free, malloc
 
 from wildboar.utils cimport stats
@@ -39,12 +39,12 @@ cdef struct Interval:
 
 
 cdef class Summarizer:
-    cdef void summarize(
-            self,
-            double *x,
-            Py_ssize_t length,
-            double *out
-    ) nogil:
+    cdef void summarize_all(self, double *x, Py_ssize_t length, double *out) nogil:
+        cdef Py_ssize_t i
+        for i in range(self.n_outputs()):
+            out[i] = self.summarize(i, x, length)
+
+    cdef double summarize(self, Py_ssize_t i, double *x, Py_ssize_t length) nogil:
         pass
 
     cdef void reset(self, Dataset td) nogil:
@@ -53,81 +53,41 @@ cdef class Summarizer:
     cdef Py_ssize_t n_outputs(self) nogil:
         return -1
 
-cdef class MeanSummarizer(Summarizer):
-    cdef void summarize(
-            self,
-            double *x,
-            Py_ssize_t length,
-            double *out
-    ) nogil:
-        out[0] = stats.mean(x, length)
 
+cdef class MeanSummarizer(Summarizer):
+    cdef double summarize(self, Py_ssize_t i, double *x, Py_ssize_t length) nogil:
+        return stats.mean(x, length)
+        
     cdef Py_ssize_t n_outputs(self) nogil:
         return 1
 
 
 cdef class VarianceSummarizer(Summarizer):
-    cdef void summarize(
-            self,
-            double *x,
-            Py_ssize_t length,
-            double *out
-    ) nogil:
-        out[0] = stats.variance(x, length)
+    cdef double summarize(self, Py_ssize_t i, double *x, Py_ssize_t length) nogil:
+        return stats.variance(x, length)
 
     cdef Py_ssize_t n_outputs(self) nogil:
         return 1
 
 
 cdef class SlopeSummarizer(Summarizer):
-    cdef void summarize(
-            self,
-            double *x,
-            Py_ssize_t length,
-            double *out
-    ) nogil:
-        out[0] = stats.slope(x, length)
+    cdef double summarize(self, Py_ssize_t i, double *x, Py_ssize_t length) nogil:
+        return stats.slope(x, length)
 
     cdef Py_ssize_t n_outputs(self) nogil:
         return 1
 
 
-cdef class MultiSummarizer(Summarizer):
-
-    cdef void summarize(
-            self,
-            double *x,
-            Py_ssize_t length,
-            double *out
-    ) nogil:
-        cdef Py_ssize_t i
-        for i in range(self.n_outputs()):
-            out[i] = self._compute(i, x, length)
-
-    cdef double _compute(
-        self,
-        Py_ssize_t measure,
-        double *x,
-        Py_ssize_t length
-    ) nogil:
-        return 0
-
-
-cdef class MeanVarianceSlopeSummarizer(MultiSummarizer):
-    cdef double _compute(
-            self,
-            Py_ssize_t measure,
-            double *x,
-            Py_ssize_t length
-    ) nogil:
-        cdef Py_ssize_t i
-        cdef Py_ssize_t v
-        if measure == 0:
+cdef class MeanVarianceSlopeSummarizer(Summarizer):
+    cdef double summarize(self, Py_ssize_t i, double *x, Py_ssize_t length) nogil:
+        if i == 0:
             return stats.mean(x, length)
-        elif measure == 1:
+        elif i == 1:
             return stats.variance(x, length)
-        elif measure == 2:
+        elif i == 2:
             return stats.slope(x, length)
+        else:
+            return NAN
 
     cdef Py_ssize_t n_outputs(self) nogil:
         return 3
@@ -201,47 +161,71 @@ cdef class Catch22Summarizer(Summarizer):
     def __reduce__(self):
         return self.__class__, ( )
 
-    cdef void summarize(
-            self,
-            double *x,
-            Py_ssize_t length,
-            double *out
-    ) nogil:
-        cdef Py_ssize_t welch_length = stats.next_power_of_2(length)
-        cdef int n_welch = stats.welch(
-            x, 
-            length, 
-            welch_length, 
-            1.0, 
-            self.window, 
-            length, 
-            self.welch_s, 
-            self.welch_f
-        )
-        stats.auto_correlation(x, length, self.ac)
+    # This is ugly
+    cdef double summarize(self, Py_ssize_t i, double *x, Py_ssize_t length) nogil:
+        cdef Py_ssize_t welch_length = -1
+        cdef int n_welch = -1
+        
+        if i == 15 or i == 20:    
+            n_welch = stats.welch(
+                x, 
+                length, 
+                stats.next_power_of_2(length), 
+                1.0, 
+                self.window, 
+                length, 
+                self.welch_s, 
+                self.welch_f
+            )
+        if i == 2 or i == 3 or i == 8 or i == 10 or i == 12:
+            stats.auto_correlation(x, length, self.ac)
 
-        out[0] = _catch22.histogram_mode5(x, length, self.bin_count, self.bin_edges)
-        out[1] = _catch22.histogram_mode10(x, length, self.bin_count, self.bin_edges)
-        out[2] = _catch22.f1ecac(self.ac, length)
-        out[3] = _catch22.first_min(self.ac, length)
-        out[4] = _catch22.histogram_ami_even_2_5(x, length)
-        out[5] = _catch22.trev_1_num(x, length)
-        out[6] = _catch22.hrv_classic_pnn(x, length, 40)
-        out[7] = _catch22.above_mean_stretch(x, length)
-        out[8] = _catch22.transition_matrix_3ac_sumdiagcov(x, self.ac, length)
-        out[9] = _catch22.periodicity_wang_th0_01(x, length)
-        out[10] = _catch22.embed2_dist_tau_d_expfit_meandiff(x, self.ac, length)
-        out[11] = _catch22.auto_mutual_info_stats_gaussian_fmmi(x, length, 40)
-        out[12] = _catch22.local_mean_tauresrat(x, self.ac, length, 1)
-        out[13] = _catch22.outlier_include_np_mdrmd(x, length, 1, 0.01)
-        out[14] = _catch22.outlier_include_np_mdrmd(x, length, -1, 0.01)
-        out[15] = _catch22.summaries_welch_rect(x, length, 1, self.welch_s, self.welch_f, n_welch)
-        out[16] = _catch22.below_diff_stretch(x, length)
-        out[17] = _catch22.motif_three_quantile_hh(x, length)
-        out[18] = _catch22.fluct_anal_2_50_1_logi_prop_r1(x, length, 1, 0)
-        out[19] = _catch22.fluct_anal_2_50_1_logi_prop_r1(x, length, 2, 1)
-        out[20] = _catch22.summaries_welch_rect(x, length, 0, self.welch_s, self.welch_f, n_welch)
-        out[21] = _catch22.local_mean_std(x, length, 3)
+        if i == 0:
+            return _catch22.histogram_mode5(x, length, self.bin_count, self.bin_edges)
+        elif i == 1:
+            return _catch22.histogram_mode10(x, length, self.bin_count, self.bin_edges)
+        elif i == 2:
+            return _catch22.f1ecac(self.ac, length)
+        elif i == 3:
+            return _catch22.first_min(self.ac, length)
+        elif i == 4:
+            return _catch22.histogram_ami_even_2_5(x, length)
+        elif i == 5:
+            return _catch22.trev_1_num(x, length)
+        elif i == 6:
+            return _catch22.hrv_classic_pnn(x, length, 40)
+        elif i == 7:
+            return _catch22.above_mean_stretch(x, length)
+        elif i == 8:
+            return _catch22.transition_matrix_3ac_sumdiagcov(x, self.ac, length)
+        elif i == 9:
+            return _catch22.periodicity_wang_th0_01(x, length)
+        elif i == 10:
+            return _catch22.embed2_dist_tau_d_expfit_meandiff(x, self.ac, length)
+        elif i == 11:
+            return _catch22.auto_mutual_info_stats_gaussian_fmmi(x, length, 40)
+        elif i == 12:
+            return _catch22.local_mean_tauresrat(x, self.ac, length, 1)
+        elif i == 13:
+            return _catch22.outlier_include_np_mdrmd(x, length, 1, 0.01)
+        elif i == 14:
+            return _catch22.outlier_include_np_mdrmd(x, length, -1, 0.01)
+        elif i == 15:
+            return _catch22.summaries_welch_rect(x, length, 1, self.welch_s, self.welch_f, n_welch)
+        elif i == 16:
+            return _catch22.below_diff_stretch(x, length)
+        elif i == 17:
+            return _catch22.motif_three_quantile_hh(x, length)
+        elif i == 18:
+            return _catch22.fluct_anal_2_50_1_logi_prop_r1(x, length, 1, 0)
+        elif i == 19:
+            return _catch22.fluct_anal_2_50_1_logi_prop_r1(x, length, 2, 1)
+        elif i == 20:
+            return _catch22.summaries_welch_rect(x, length, 0, self.welch_s, self.welch_f, n_welch)
+        elif i == 21:
+            return _catch22.local_mean_std(x, length, 3)
+        else:
+            return NAN
 
     cdef Py_ssize_t n_outputs(self) nogil:
         return 22
@@ -261,7 +245,7 @@ cdef class PyFuncSummarizer(Summarizer):
         with gil:
             self.x_buffer = np.ndarray(td.n_timestep)
 
-    cdef void summarize(
+    cdef void summarize_all(
             self,
             double *x,
             Py_ssize_t length,
@@ -278,6 +262,15 @@ cdef class PyFuncSummarizer(Summarizer):
                 value = self.func[i](self.x_buffer[0:length])
             out[i] = value
 
+    cdef double summarize(self, Py_ssize_t m, double *x, Py_ssize_t length) nogil:
+        cdef double value
+        cdef Py_ssize_t i
+        with gil:
+            for i in range(length):
+                self.x_buffer[i] = x[i]
+            value = self.func[m](self.x_buffer[0:length])
+        return value
+
     cdef Py_ssize_t n_outputs(self) nogil:
         with gil:
             return len(self.func)
@@ -290,15 +283,10 @@ cdef inline Py_ssize_t _imin(Py_ssize_t a, Py_ssize_t b) nogil:
 cdef class IntervalFeatureEngineer(FeatureEngineer):
     cdef Py_ssize_t n_intervals
     cdef Summarizer summarizer
-    cdef double *out_buffer
 
     def __cinit__(self, Py_ssize_t n_intervals, Summarizer summarizer, *args, **kwargs):
         self.n_intervals = n_intervals
         self.summarizer = summarizer
-        self.out_buffer = <double *> malloc(sizeof(double) * summarizer.n_outputs())
-
-    def __dealloc__(self):
-        free(self.out_buffer)
 
     def __reduce__(self):
         return self.__class__, (self.n_intervals, self.summarizer)
@@ -371,12 +359,11 @@ cdef class IntervalFeatureEngineer(FeatureEngineer):
             Py_ssize_t sample
     ) nogil:
         cdef Interval *interval = <Interval*> feature.feature
-        self.summarizer.summarize(
+        return self.summarizer.summarize(
+            interval.random_output,
             td.get_sample(sample, feature.dim) + interval.start,
             interval.length,
-            self.out_buffer,
         )
-        return self.out_buffer[interval.random_output]
 
     cdef double persistent_feature_value(
             self,
@@ -397,7 +384,7 @@ cdef class IntervalFeatureEngineer(FeatureEngineer):
     ) nogil:
         cdef Py_ssize_t n_summarizers = self.summarizer.n_outputs()
         cdef Interval *interval = <Interval*> feature.feature
-        self.summarizer.summarize(
+        self.summarizer.summarize_all(
             td.get_sample(sample, feature.dim) + interval.start,
             interval.length,
             td_out.get_sample(out_sample) + out_feature * n_summarizers,
