12200a11903ce487d6bd5562b044f4f22bfbf92e
isaksamsten <isaksamsten@users.noreply.github.com>
2021-11-29 21:00:38 +0000

    deploy: 806ebbf857cbc1f6fe26def78f79019d3cc18964

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.0rc4.dev36+ga0b4b7f5',
+    VERSION: '1.1.0rc4.dev38+g806ebbf8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
