476fe49e15c70958b7ee89ffdd48a593334ef50a
Isak Samsten <isak@samsten.se>
2021-12-23 00:21:00 +0100

    Add random samling using Walker-Vose

--- src/wildboar/utils/rand.pxd
+++ src/wildboar/utils/rand.pxd
@@ -20,6 +20,19 @@
 cdef enum:
     RAND_R_MAX = 2147483647
 
+cdef struct VoseRand:
+    Py_ssize_t n
+    Py_ssize_t *alias
+    double *prob
+
+cdef void vose_rand_init(VoseRand *vr, Py_ssize_t n) nogil
+
+cdef void vose_rand_free(VoseRand *vr) nogil
+
+cdef void vose_rand_precompute(VoseRand *vr, double *p) nogil
+
+cdef Py_ssize_t vose_rand_int(VoseRand *vr, size_t *seed) nogil
+
 cdef size_t rand_r(size_t *seed) nogil
 
 cdef size_t rand_int(size_t min_val, size_t max_val, size_t *seed) nogil
--- src/wildboar/utils/rand.pyx
+++ src/wildboar/utils/rand.pyx
@@ -19,6 +19,111 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from libc.math cimport log, sqrt
+from libc.stdlib cimport free, malloc
+
+
+# https://jugit.fz-juelich.de/mlz/ransampl/-/blob/master/lib/ransampl.c
+cdef void vose_rand_init(VoseRand *vr, Py_ssize_t n) nogil:
+    vr.prob = <double*> malloc(sizeof(double) * n)
+    vr.alias = <Py_ssize_t*> malloc(sizeof(Py_ssize_t) * n)
+    vr.n = n
+
+cdef void vose_rand_free(VoseRand *vr) nogil:
+    free(vr.prob)
+    free(vr.alias)
+
+cdef void vose_rand_precompute(VoseRand *vr, double *p) nogil:
+    cdef Py_ssize_t n = vr.n
+    cdef Py_ssize_t i, a, g
+    cdef double *P = <double*> malloc(sizeof(double) * n)
+    cdef Py_ssize_t *S = <Py_ssize_t*> malloc(sizeof(Py_ssize_t) * n)
+    cdef Py_ssize_t *L = <Py_ssize_t*> malloc(sizeof(Py_ssize_t) * n)
+
+    cdef double s = 0
+    for i in range(n):
+        s += p[i]
+
+    for i in range(n):
+        P[i] = p[i] * n / s
+
+    cdef Py_ssize_t nS = 0
+    cdef Py_ssize_t nL = 0
+    for i in range(n - 1, -1, -1):
+        if P[i] < 1:
+            S[nS] = i
+            nS += 1
+        else:
+            L[nL] = i
+            nL += 1
+
+    while nS > 0 and nL > 0: 
+        nS -= 1
+        a = S[nS]
+        nL -= 1
+        g = L[nL]
+        vr.prob[a] = P[a]
+        vr.alias[a] = g
+        P[g] = P[g] + P[a] - 1
+        if P[g] < 1:
+            S[nS] = g
+            nS += 1
+        else:
+            L[nL] = g
+            nL += 1
+
+    while nL > 0:
+        nL -= 1
+        vr.prob[L[nL]] = 1
+
+    while nS > 0:
+        nS -= 1
+        vr.prob[S[nS]] = 1
+
+    free(P)
+    free(S)
+    free(L)
+
+
+cdef Py_ssize_t vose_rand_int(VoseRand *vr, size_t *seed) nogil:
+    cdef double r1 = rand_uniform(0, 1, seed)
+    cdef double r2 = rand_uniform(0, 1, seed)
+    cdef Py_ssize_t i = <Py_ssize_t> (vr.n * r1)
+    if r2 < vr.prob[i]:
+        return i 
+    else:
+        return vr.alias[i]
+
+
+from libc.stdio cimport printf
+
+
+def test(r):
+    cdef Py_ssize_t i
+    cdef VoseRand vr
+    vose_rand_init(&vr, 10)
+    cdef double *p = <double*> malloc(sizeof(double) * 10)
+    p[0] = 0.5
+    for i in range(9):
+        p[i + 1] = 0.5 / 9
+    for i in range(10):
+        printf("p[%d]=%f\n", i, p[i])
+
+    vose_rand_precompute(&vr, p)
+
+    cdef size_t seed = 102
+    cdef Py_ssize_t k
+    import numpy as np
+    arr = np.zeros(r, dtype=np.intp)
+    for i in range(r):
+        k = vose_rand_int(&vr, &seed)
+        if k < 0:
+            printf("wtf!!!\n")
+        if k > 9:
+            printf("wtf2!!!\n")
+        #printf("%d=%d\n", i, k)
+        arr[i] = k
+
+    return arr
 
 
 cdef inline size_t rand_r(size_t *seed) nogil:
