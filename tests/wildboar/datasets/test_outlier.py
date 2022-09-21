0ab6b875f489dd4f041b8b2483d00c0617d0dfe6
Isak Samsten <isak@samsten.se>
2022-09-21 14:08:33 +0200

    Fix outlier dataset tests

--- tests/wildboar/datasets/test_outlier.py
+++ tests/wildboar/datasets/test_outlier.py
@@ -15,79 +15,55 @@
 #
 # Authors: Isak Samsten
 
-import math
-
 import numpy as np
-from numpy.testing import assert_allclose, assert_, assert_equal
-from sklearn.utils import check_random_state
+import pytest
+from numpy.testing import assert_almost_equal, assert_equal
 
-from wildboar.datasets.outlier import MinorityLabeler, EmmottLabeler
-
-
-def _new_labeler_data(n_samples, n_timestep, classes, n_classes):
-    random_state = check_random_state(123)
-    x = random_state.randn(n_samples, n_timestep)
-    y = np.repeat(classes, n_classes)
-    return x, y
+from wildboar.datasets import load_dataset
+from wildboar.datasets.outlier import minority_outliers
 
 
-def _run_labeler(labeler, x, y):
-    new_x, new_y = labeler.fit_transform(x, y)
-    new_x_1, new_y_1 = labeler.transform(x, y)
-    return new_x, new_x_1, new_y, new_y_1
+@pytest.mark.parametrize(
+    "n_outliers, expected_n_outliers, "
+    "expected_first_outlier_sample, expected_outlier_norm",
+    [
+        (0.05, 5, 18, 27.294688068996287),
+        (0.1, 10, 18, 38.60051804670847),
+        (0.2, 20, 18, 54.58937618813437),
+    ],
+)
+def test_minority_outliers(
+    n_outliers,
+    expected_n_outliers,
+    expected_first_outlier_sample,
+    expected_outlier_norm,
+):
+    x, y = load_dataset("GunPoint", repository="wildboar/ucr-tiny")
+    x_outlier, y_outlier = minority_outliers(
+        x, y, n_outliers=n_outliers, random_state=123
+    )
+    assert_outliers(
+        x,
+        x_outlier,
+        y_outlier,
+        expected_n_outliers=expected_n_outliers,
+        expected_first_outlier_sample=expected_first_outlier_sample,
+        expected_outlier_norm=expected_outlier_norm,
+    )
 
 
-class SetupLabeler:
-    def test_binary(self):
-        x, y = _new_labeler_data(20, 10, [1, 2], [15, 5])
-        self._run_labeler_suite(x, y, *(np.unique(y, return_counts=True)))
-
-    def test_multiclass(self):
-        x, y = _new_labeler_data(20, 10, [1, 2, 3], [8, 7, 5])
-        self._run_labeler_suite(x, y, *(np.unique(y, return_counts=True)))
-
-    def _run_labeler_suite(self, x, y, classes, label_count):
-        self._run_labeler_n_outliers(x, y, classes, label_count, 0.05)
-        self._run_labeler_n_outliers(x, y, classes, label_count, 0.1)
-
-    def _run_labeler_n_outliers(self, x, y, classes, label_count, n_outliers):
-        new_x, new_x_1, new_y, new_y_1 = _run_labeler(
-            self._new_labeler(n_outliers), x, y
-        )
-        assert_allclose(new_x, new_x_1)
-        assert_allclose(new_y, new_y_1)
-
-        self._assert_labeler(new_x, new_y, classes, label_count, n_outliers)
-
-    def _new_labeler(self, n_outliers):
-        raise NotImplemented()
-
-    def _assert_labeler(self, new_x, new_y, classes, label_count, n_outliers):
-        raise NotImplemented()
-
-
-class TestMinorityLabeler(SetupLabeler):
-    def _new_labeler(self, n_outliers):
-        return MinorityLabeler(n_outliers=n_outliers, random_state=1)
-
-    def _assert_labeler(self, x, y, classes, label_count, n_outliers):
-        minority_label_index = np.argmin(label_count)
-        minority_label = classes[minority_label_index]
-        majority_label_count = np.sum(label_count[classes != minority_label])
-        actual_n_outliers = math.ceil(majority_label_count * n_outliers)
-
-        n_samples = majority_label_count + actual_n_outliers
-        y_outlier = -np.ones(actual_n_outliers)
-        y_inlier = np.ones(majority_label_count)
-
-        assert_equal(x.shape, [n_samples, x.shape[1]])
-        assert_equal(y.shape, [n_samples])
-        assert_allclose(y, np.concatenate([y_outlier, y_inlier]))
-
-
-class TestEmmottLabeler(SetupLabeler):
-    def _new_labeler(self, n_outliers):
-        return EmmottLabeler(n_outliers=n_outliers, difficulty="any", random_state=1)
-
-    def _assert_labeler(self, x, y, classes, label_count, n_outliers):
-        pass
+def assert_outliers(
+    x,
+    x_outlier,
+    y_outlier,
+    *,
+    expected_n_outliers,
+    expected_first_outlier_sample,
+    expected_outlier_norm
+):
+    actual_n_outliers = np.where(y_outlier == -1)[0].shape[0]
+    actual_fist_outlier_sample = (np.sum(x_outlier[0] - x, axis=1) == 0).argmax()
+    actual_outlier_norm = np.linalg.norm(x_outlier[np.where(y_outlier == -1)])
+    assert_almost_equal(actual_outlier_norm, expected_outlier_norm)
+    assert_equal(actual_fist_outlier_sample, expected_first_outlier_sample)
+    assert_equal(actual_n_outliers, expected_n_outliers)
