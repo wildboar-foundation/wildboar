67506fe7ffb079f214afb7854f43efe69ebfadf5
Isak Samsten <isak@samsten.se>
2022-10-04 15:07:30 +0200

    Add tests for subsequence_match

--- tests/wildboar/distance/test_distance.py
+++ tests/wildboar/distance/test_distance.py
@@ -4,12 +4,13 @@
 import pytest
 from numpy.testing import assert_almost_equal, assert_equal
 
-from wildboar.datasets import load_dataset
+from wildboar.datasets import load_gun_point
 from wildboar.distance import (
     paired_distance,
     paired_subsequence_distance,
     pairwise_distance,
     pairwise_subsequence_distance,
+    subsequence_match,
 )
 
 
@@ -71,7 +72,7 @@ from wildboar.distance import (
     ],
 )
 def test_pairwise_dtw_distance(r, expected):
-    x, y = load_dataset("GunPoint", repository="wildboar/ucr-tiny")
+    x, y = load_gun_point()
     assert_almost_equal(
         pairwise_distance(x[0:3], x[10:15], metric="dtw", metric_params={"r": r}),
         expected,
@@ -102,7 +103,7 @@ def test_pairwise_dtw_distance(r, expected):
     ],
 )
 def test_paired_dtw_distance(r, expected):
-    x, y = load_dataset("GunPoint", repository="wildboar/ucr-tiny")
+    x, y = load_gun_point()
     assert_almost_equal(
         paired_distance(x[0:3], x[30:33], metric="dtw", metric_params={"r": r}),
         expected,
@@ -169,7 +170,7 @@ def test_paired_dtw_distance(r, expected):
 def test_pairwise_subsequence_distance(
     metric, metric_params, expected_min_dist, expected_min_ind
 ):
-    x, y = load_dataset("GunPoint", repository="wildboar/ucr-tiny")
+    x, y = load_gun_point()
     min_dist, min_ind = pairwise_subsequence_distance(
         x[[2, 3], 0:20],
         x[40:45],
@@ -217,7 +218,7 @@ def test_pairwise_subsequence_distance(
 def test_paired_subsequence_distance(
     metric, metric_params, expected_min_dist, expected_min_ind
 ):
-    x, y = load_dataset("GunPoint", repository="wildboar/ucr-tiny")
+    x, y = load_gun_point()
     min_dist, min_ind = paired_subsequence_distance(
         x[[2, 3, 8], 0:20],
         x[40:43],
@@ -227,3 +228,198 @@ def test_paired_subsequence_distance(
     )
     assert_almost_equal(min_dist, expected_min_dist)
     assert_equal(min_ind, expected_min_ind)
+
+
+@pytest.mark.parametrize(
+    "metric, metric_params, expected_indicies, expected_dists",
+    [
+        [
+            "euclidean",
+            {},
+            [
+                [126, 127, 125, 128, 129, 130, 124, 131, 132, 123],
+                [109, 110, 108, 111, 138, 118, 117, 124, 119, 112],
+            ],
+            [
+                [
+                    0.03397965606582041,
+                    0.0343842104203779,
+                    0.0351323095527136,
+                    0.03539658207798578,
+                    0.03645583018956124,
+                    0.036979988389006124,
+                    0.03703322511321164,
+                    0.03831106381122556,
+                    0.04012265023031157,
+                    0.04104258653686285,
+                ],
+                [
+                    0.18783105110608192,
+                    0.1962122726590744,
+                    0.19969665170517434,
+                    0.21358058104153474,
+                    0.2286672665170618,
+                    0.23024263523833352,
+                    0.2310810578541956,
+                    0.2323035020498981,
+                    0.23286074160686906,
+                    0.23352672398649205,
+                ],
+            ],
+        ],
+        [
+            "scaled_euclidean",
+            {},
+            [
+                [90, 89, 83, 91, 84, 82, 85, 88, 81, 92],
+                [75, 89, 88, 87, 71, 90, 86, 76, 70, 72],
+            ],
+            [
+                [
+                    0.5171341149025254,
+                    0.6106690561241724,
+                    0.6199414822706423,
+                    0.6389336609057809,
+                    0.6861095369454515,
+                    0.6919642938064638,
+                    0.7518358747794223,
+                    0.7551918338623284,
+                    0.8089276966112849,
+                    0.8305581115853162,
+                ],
+                [
+                    0.5395380605368274,
+                    0.5994517516083463,
+                    0.6060069282120507,
+                    0.6572386395006412,
+                    0.6865909258011326,
+                    0.7388046565921842,
+                    0.7530885210085916,
+                    0.810795863777238,
+                    0.8376980330763003,
+                    0.8501658606924453,
+                ],
+            ],
+        ],
+        [
+            "dtw",
+            {"r": 0.5},
+            [
+                [126, 127, 125, 128, 129, 130, 124, 131, 132, 123],
+                [109, 110, 108, 125, 124, 116, 111, 117, 126, 118],
+            ],
+            [
+                [
+                    0.03377513034186882,
+                    0.03417138314023305,
+                    0.0351323095527136,
+                    0.035180236615724184,
+                    0.03620168568738906,
+                    0.03692724340632057,
+                    0.037000014085456216,
+                    0.03829773823788508,
+                    0.04012265023031157,
+                    0.04092858833956632,
+                ],
+                [
+                    0.1859017766089558,
+                    0.19554091347043767,
+                    0.19601154714547592,
+                    0.2090317042574604,
+                    0.21199763600964866,
+                    0.2124628268335682,
+                    0.21347216622866322,
+                    0.2137223700636814,
+                    0.2152617975710072,
+                    0.21833681247621853,
+                ],
+            ],
+        ],
+        [
+            "scaled_dtw",
+            {"r": 0.5},
+            [
+                [90, 84, 86, 92, 83, 87, 89, 85, 88, 91],
+                [75, 72, 71, 89, 90, 88, 74, 87, 91, 77],
+            ],
+            [
+                [
+                    0.5171341149025254,
+                    0.53438801033894,
+                    0.5449097439782348,
+                    0.5815724724804437,
+                    0.5868331003736715,
+                    0.5874821636080592,
+                    0.5972173148623663,
+                    0.5978139524706985,
+                    0.613990263275831,
+                    0.6158445792495065,
+                ],
+                [
+                    0.40792177885373476,
+                    0.49151144102128486,
+                    0.5507076093071009,
+                    0.5574072024166825,
+                    0.5643688082122642,
+                    0.5774972865470762,
+                    0.5904850517319787,
+                    0.6204986495558397,
+                    0.6239597262924556,
+                    0.6674760578321446,
+                ],
+            ],
+        ],
+    ],
+)
+def test_subseqence_match_default(
+    metric, metric_params, expected_indicies, expected_dists
+):
+    x, _ = load_gun_point()
+    subsequence = x[0, 3:15]
+    actual_indicies, actual_dists = subsequence_match(
+        subsequence,
+        x[1:3],
+        return_distance=True,
+        metric=metric,
+        metric_params=metric_params,
+    )
+
+    assert_almost_equal(actual_dists, expected_dists)
+    assert_equal(actual_indicies, expected_indicies)
+
+
+@pytest.mark.parametrize(
+    "left, right",
+    [
+        ["euclidean", "dtw"],
+        ["scaled_euclidean", "scaled_dtw"],
+        ["scaled_euclidean", "mass"],
+        ["mass", "scaled_dtw"],
+    ],
+)
+def test_subsequence_match_equivalent(left, right):
+    x, _ = load_gun_point()
+    subsequence = x[10, 40:60]
+
+    left_indicies, left_distances = subsequence_match(
+        subsequence,
+        x[0:9],
+        return_distance=True,
+        max_matches=20,
+        exclude=4,
+        metric=left,
+        metric_params=dict(r=0.0) if "dtw" in left else {},
+    )
+
+    right_indicies, right_distances = subsequence_match(
+        subsequence,
+        x[0:9],
+        return_distance=True,
+        max_matches=20,
+        exclude=4,
+        metric=right,
+        metric_params=dict(r=0.0) if "dtw" in right else {},
+    )
+
+    assert_almost_equal(left_distances, right_distances)
+    assert_equal(left_indicies, right_indicies)
