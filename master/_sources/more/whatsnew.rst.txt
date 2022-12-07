e5ccf5a2bf8c2082ea7310116f67c1dedf1ff967
isaksamsten <isaksamsten@users.noreply.github.com>
2022-12-07 21:08:21 +0000

    deploy: 30b7a6093ef7020eeb6f696ab67a24267ad408da

--- master/_sources/more/whatsnew.rst.txt
+++ master/_sources/more/whatsnew.rst.txt
@@ -6,6 +6,7 @@ What's new
    :maxdepth: 1
    :caption: Versions
 
+   versions/v1.2
    versions/v1.1
    versions/v1.0
    versions/older
--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.1.dev211+g4a48072a2',
+    VERSION: '1.1.1.dev214+g30b7a6093',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
