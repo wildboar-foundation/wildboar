8e58eb8c8432ce161620768ccc0c25371efb3c85
isaksamsten <isaksamsten@users.noreply.github.com>
2023-02-28 19:46:23 +0000

    deploy: 59130ef133d529f88bc7406f004e52d6c623bacf

--- master/_sources/more/contributing.rst.txt
+++
@@ -1,9 +0,0 @@
-=================
-Developer's guide
-=================
-
-Contributing
-============
-
-While the project currently has only one contributor, the aim of the project is to become
-a community effort.
--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.1.dev269',
+    VERSION: '1.1.1.dev76',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
