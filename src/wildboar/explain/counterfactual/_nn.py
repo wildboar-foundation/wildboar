61ff86687aba5c79862b7cf365d428b9233c83fe
Isak Samsten <isak@samsten.se>
2023-10-12 15:08:08 +0200

    Fix documentation

--- src/wildboar/explain/counterfactual/_nn.py
+++ src/wildboar/explain/counterfactual/_nn.py
@@ -10,7 +10,16 @@ from ...base import BaseEstimator, CounterfactualMixin, ExplainerMixin
 
 
 class KNeighborsCounterfactual(CounterfactualMixin, ExplainerMixin, BaseEstimator):
-    """Fit a counterfactual explainer to a k-nearest neighbors classifier.
+    """
+    Fit a counterfactual explainer to a k-nearest neighbors classifier.
+
+    Parameters
+    ----------
+    random_state : int or RandomState, optional
+        - If `int`, `random_state` is the seed used by the random number generator
+        - If `RandomState` instance, `random_state` is the random number generator
+        - If `None`, the random number generator is the `RandomState` instance used
+          by `np.random`.
 
     Attributes
     ----------
