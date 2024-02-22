9d4773f54fa888e3198560f6263ea6b9fceae3f5
Isak Samsten <isak@samsten.se>
2024-02-22 12:25:30 +0100

    Revert Castor classifier changes when using diff.

--- src/wildboar/linear_model/_shapelet.py
+++ src/wildboar/linear_model/_shapelet.py
@@ -5,7 +5,7 @@ import numbers
 import numpy as np
 from joblib import effective_n_jobs
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.utils import _is_arraylike_not_scalar, check_random_state
+from sklearn.utils import check_random_state
 
 from ..datasets.preprocess import SparseScaler
 from ..transform import (
@@ -257,8 +257,8 @@ class CastorClassifier(TransformRidgeClassifierCV):
     normalize_prob : float, optional
         The probability of standardizing a shapelet with zero mean and unit
         standard deviation.
-    shapelet_size : int or array-like, optional
-        The length of the dilated shapelet. If list, use multiple shapelet sizes.
+    shapelet_size : int, optional
+        The length of the dilated shapelet.
     lower : float, optional
         The lower percentile to draw distance thresholds above.
     upper : float, optional
@@ -266,12 +266,18 @@ class CastorClassifier(TransformRidgeClassifierCV):
     order : int or array-like, optional
         The order of difference.
 
-        - If int, half the groups with corresponding shapelets will be
-          convolved with the `order` discrete difference along the time
-          dimension.
-        - If array-like, include `n_groups` dialated shapelet groups
-          for each discrete difference of order `order[i]` in the final
-          transform.
+        If int, half the groups with corresponding shapelets will be convolved
+        with the `order` discrete difference along the time dimension.
+    soft_min : bool, optional
+        If `True`, use the sum of minimal distances. Otherwise, use the count
+        of minimal distances.
+    soft_max : bool, optional
+        If `True`, use the sum of maximal distances. Otherwise, use the count
+        of maximal distances.
+    soft_threshold : bool, optional
+        If `True`, count the time steps below the threshold for all shapelets.
+        Otherwise, count the time steps below the threshold for the shapelet
+        with the minimal distance.
     alphas : array-like of shape (n_alphas,), optional
         Array of alpha values to try.
     fit_intercept : bool, optional
@@ -376,80 +382,32 @@ class CastorClassifier(TransformRidgeClassifierCV):
             and self.order > 0
             and self.n_groups > 1
         ):
-            if not _is_arraylike_not_scalar(self.shapelet_size):
-                shapelet_size = [self.shapelet_size]
-            else:
-                shapelet_size = self.shapelet_size
+            n_groups = self.n_groups // 2
+            union_n_jobs = n_jobs // n_groups
+            if union_n_jobs < 1:
+                union_n_jobs = None
 
-            n_groups = self.n_groups // 2 // len(shapelet_size)
-            if n_groups < 2:
-                raise ValueError(
-                    "unsupported shapelet_size, n_groups // 2 // len(shapelet_size) "
-                    "must be larger than 1."
-                )
-            extra = self.n_groups % len(shapelet_size)
-
-            union = []
-            for i, size in enumerate(shapelet_size):
-                ng = n_groups
-                if i < extra:
-                    ng += 1
-
-                union.append(
+            return make_union(
+                CastorTransform(
+                    n_groups=n_groups,
+                    shapelet_size=self.shapelet_size,
+                    random_state=random_state.randint(np.iinfo(np.int32).max),
+                    **params,
+                ),
+                make_pipeline(
+                    DiffTransform(order=self.order),
                     CastorTransform(
-                        n_groups=ng,
-                        shapelet_size=size,
+                        n_groups=n_groups,
+                        shapelet_size=self.shapelet_size,
                         random_state=random_state.randint(np.iinfo(np.int32).max),
                         **params,
                     ),
-                )
-                union.append(
-                    make_pipeline(
-                        DiffTransform(order=self.order),
-                        CastorTransform(
-                            n_groups=ng,
-                            shapelet_size=size,
-                            random_state=random_state.randint(np.iinfo(np.int32).max),
-                            **params,
-                        ),
-                    ),
-                )
-            return make_union(*union, n_jobs=max(1, n_jobs // n_groups))
+                ),
+            )
         else:
-            if not _is_arraylike_not_scalar(self.shapelet_size):
-                shapelet_size = [self.shapelet_size]
-            else:
-                shapelet_size = self.shapelet_size
-
-            if len(shapelet_size) == 1:
-                return CastorTransform(
-                    n_groups=self.n_groups,
-                    shapelet_size=shapelet_size[0],
-                    random_state=random_state,
-                    **params,
-                )
-            else:
-                n_groups = self.n_groups // len(shapelet_size)
-                if n_groups < 1:
-                    raise ValueError(
-                        "unsupported shapelet_size, n_groups // len(shapelet_size) "
-                        "must be larger than 0."
-                    )
-
-                extra = self.n_groups % len(self.shapelet_size)
-                union = []
-                for i, size in enumerate(shapelet_size):
-                    ng = n_groups
-                    if i < extra:
-                        ng += 1
-
-                    union.append(
-                        CastorTransform(
-                            n_groups=ng,
-                            shapelet_size=size,
-                            random_state=random_state.randint(np.iinfo(np.int32).max),
-                            **params,
-                        )
-                    )
-
-                return make_union(*union, n_jobs=max(1, n_jobs // n_groups))
+            return CastorTransform(
+                n_groups=self.n_groups,
+                shapelet_size=self.shapelet_size,
+                random_state=random_state,
+                **params,
+            )
