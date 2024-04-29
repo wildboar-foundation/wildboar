d6a09518e3102691659a9db64024a07a19abfabe
isaksamsten <isaksamsten@users.noreply.github.com>
2024-04-29 12:34:28 +0000

    deploy: c77a031b453faa0c066b7299ef1e90bb4debbf5d

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.3.0.dev56+gb6bf824b5',
+    VERSION: '1.3.0.dev58+gc77a031b4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
