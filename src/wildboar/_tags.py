b2dbb1709a098cb935e24fad9f61dbdf4cf2958d
Isak Samsten <isak@samsten.se>
2025-03-28 15:04:39 +0100

    Add missing _tags.py file

---
+++ src/wildboar/_tags.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from sklearn.utils._tags import Tags, _dataclass_args
+
+
+@dataclass(**_dataclass_args())
+class ExplainerTags:
+    require_estimator: bool = True
+
+
+@dataclass(**_dataclass_args())
+class WildboarTags(Tags):
+    allow_eos: bool = False
+    explainer_tags: ExplainerTags | None = None
+
+
+def _tags_from_sklearn(tags: Tags) -> WildboarTags:
+    sklearn_tags = {slot: getattr(tags, slot) for slot in Tags.__slots__}
+    return WildboarTags(explainer_tags=None, **sklearn_tags)
